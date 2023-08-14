7db456f72159c9a61ef268fe31f9b55aa75e98c1
Travis Mehrer <travis@canerduh.com>
2023-08-14 15:53:23 -0600

    Exercises 01 - 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let returnString = "";
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        returnString += str[i];
+    } 
+
+    return returnString;
 };
 
 // Do not edit below this line
