34883192493d1097be0938a9ce23f51475cb9307
Travis Mehrer <travis@canerduh.com>
2023-09-08 15:40:33 -0600

    Exercise 04 initial work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inArray, toRemove) {
+    // remove specific items from array
+    let beforeRemoveElement = inArray.slice(0, (toRemove - 1));
 
+    let afterRemoveElement = inArray(toRemove);
+
+    let outArrayWithoutElement = beforeRemoveElement.concat(afterRemoveElement);
+
+    return outArrayWithoutElement;
 };
 
 // Do not edit below this line
